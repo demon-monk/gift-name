2483de96df292b2d54b6ee7ab71e1ccda1d99527
Bang Zhang <bang.zhang@liulishuo.com>
2018-12-20 00:11:29 +0800

    add speaker component

---
+++ src/components/Speaker.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import './Speaker.less'
+export default class Speaker extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+        this.myRef = React.createRef()
+    }
+
+    componentDidMount () {
+        this.audioDom = this.myRef.current.getElementsByTagName('audio')[0]
+    }
+
+    onClick = () => {
+        this.audioDom.play()
+    }
+
+    render () {
+        return (
+            <div className="comp-speaker" ref={this.myRef} onClick={this.onClick}>
+                <audio src={this.props.src} />
+            </div>
+        )
+    }
+}
